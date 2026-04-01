6f4d5a89a69717dec5bf18c7359ff4c80de44b71
Owen Carey <37121709+owenthcarey@users.noreply.github.com>
2026-04-01 11:01:03 -0700

    chore: add new generator crates to publish script

--- crates/weaveffi-cli/tests/publish_script.rs
+++ crates/weaveffi-cli/tests/publish_script.rs
@@ -39,6 +39,9 @@ fn publish_script_contains_all_publishable_crates() {
         "weaveffi-gen-wasm",
         "weaveffi-gen-python",
         "weaveffi-gen-dotnet",
+        "weaveffi-gen-cpp",
+        "weaveffi-gen-dart",
+        "weaveffi-gen-go",
         "weaveffi-gen-ruby",
         "weaveffi-cli",
     ];
